e0d7fe25a1fd984e8736e02f633979bb00eae570
Nipun Grover <nipungrover2003@outlook.com>
2026-04-15 04:00:47 -0400

    RoomsPage component displays room details and features, placeholder values for now

--- src/pages/RoomsPage.tsx
+++ src/pages/RoomsPage.tsx
@@ -1,7 +1,40 @@
+import { Link } from "@tanstack/react-router";
+
 type Props = {};
 
+const rooms = [
+  {
+    id: "a101",
+    name: "Room A101",
+    capacity: 6,
+    features: ["Projector", "Whiteboard"],
+  },
+  {
+    id: "f105",
+    name: "Room F105",
+    capacity: 10,
+    features: ["TV", "Conference Phone"],
+  },
+];
+
 function RoomsPage(props: Props) {
-  return <div>RoomsPage</div>;
+  return (
+    <div>
+      <h1>Rooms</h1>
+      {rooms.map((room) => (
+        <Link to={`/rooms/${room.id}`} key={room.id}>
+          <div>
+            <h2>{room.name}</h2>
+            <p>Capacity: {room.capacity}</p>
+
+            {room.features.map((feature, index) => (
+              <div key={index}>{feature}</div>
+            ))}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default RoomsPage;
