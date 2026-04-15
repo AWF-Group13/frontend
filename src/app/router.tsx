a8382d568e1bdab2b29c9e9d3d4c079719cbfc3d
Nipun Grover <nipungrover2003@outlook.com>
2026-04-14 22:37:14 -0400

    added private routes in the router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,6 +2,7 @@ import {
   createRootRoute,
   createRoute,
   createRouter,
+  redirect,
 } from "@tanstack/react-router";
 
 import AppShell from "../pages/AppShell";
@@ -19,6 +20,30 @@ const layoutRoute = createRoute({
   component: AppShell,
 });
 
+const protectedRoute = createRoute({
+  getParentRoute: () => layoutRoute,
+  id: "protected",
+  beforeLoad: () => {
+    const isSignedIn = true; // temporary for now
+
+    if (!isSignedIn) {
+      throw redirect({ to: "/" });
+    }
+  },
+});
+
+const adminRoute = createRoute({
+  getParentRoute: () => protectedRoute,
+  id: "admin",
+  beforeLoad: () => {
+    const role = "admin"; // temporary for now
+
+    if (role !== "admin") {
+      throw redirect({ to: "/rooms" });
+    }
+  },
+});
+
 const homeRoute = createRoute({
   getParentRoute: () => layoutRoute,
   path: "/",
@@ -26,28 +51,28 @@ const homeRoute = createRoute({
 });
 
 const roomsRoute = createRoute({
-  getParentRoute: () => layoutRoute,
+  getParentRoute: () => protectedRoute,
   path: "rooms",
   component: RoomsPage,
 });
 
 /* For viewing bookings */
 const bookingsRoute = createRoute({
-  getParentRoute: () => layoutRoute,
+  getParentRoute: () => protectedRoute,
   path: "bookings",
   component: BookingsPage,
 });
 
 /* Admin room management */
 const adminRoomsRoute = createRoute({
-  getParentRoute: () => layoutRoute,
+  getParentRoute: () => adminRoute,
   path: "admin/rooms",
   component: AdminRoomsPage,
 });
 
 /* Admin view of bookings */
 const adminBookingsRoute = createRoute({
-  getParentRoute: () => layoutRoute,
+  getParentRoute: () => adminRoute,
   path: "admin/bookings",
   component: AdminBookingsPage,
 });
@@ -55,10 +80,11 @@ const adminBookingsRoute = createRoute({
 const routeTree = rootRoute.addChildren([
   layoutRoute.addChildren([
     homeRoute,
-    roomsRoute,
-    bookingsRoute,
-    adminRoomsRoute,
-    adminBookingsRoute,
+    protectedRoute.addChildren([
+      roomsRoute,
+      bookingsRoute,
+      adminRoute.addChildren([adminRoomsRoute, adminBookingsRoute]),
+    ]),
   ]),
 ]);
 
