06cd9f0d81147c911b9af866605f75de16162fa0
Tugrap <turkeraydiner@gmail.com>
2026-04-22 09:08:29 -0400

    open 24/7 box

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,11 +2,21 @@ import "./HomePage.css";
 
 function HomePage() {
   return (
-    <section className="welcome">
-      <p>OPEN 24/7</p>
-      <h1>Welcome to LibRoom</h1>
-      <p>Find a quiet place to study, meet, or work anytime.</p>
-    </section>
+    <>
+      <section className="welcome">
+        <p>OPEN 24/7</p>
+        <h1>Welcome to LibRoom</h1>
+        <p>Find a quiet place to study, meet, or work anytime.</p>
+      </section>
+      <div className="box">
+        <img src="/time.png" alt="" width="48" height="48" />
+        <div>
+          <b>Open 24/7</b>
+          <br />
+          Reserve a room anytime, day or night.
+        </div>
+      </div>
+    </>
   );
 }
 
