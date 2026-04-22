395c34361b30d4887ddc0bf44ba68af02ff487d3
Tugrap <turkeraydiner@gmail.com>
2026-04-21 23:57:48 -0400

    add welcome page and update nav colours

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,13 @@
+import "./HomePage.css";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <section className="welcome">
+      <p>OPEN 24/7</p>
+      <h1>Welcome to LibRoom</h1>
+      <p>Find a quiet place to study, meet, or work anytime.</p>
+    </section>
+  );
 }
 
 export default HomePage;
