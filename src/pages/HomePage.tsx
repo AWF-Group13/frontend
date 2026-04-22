9a4da1b3ec190349d93a2bd4c9d19ca6c591f335
Tugrap <turkeraydiner@gmail.com>
2026-04-22 09:26:12 -0400

    add quiet spaces box

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,12 +8,22 @@ function HomePage() {
         <h1>Welcome to LibRoom</h1>
         <p>Find a quiet place to study, meet, or work anytime.</p>
       </section>
-      <div className="box">
-        <img src="/time.png" alt="" width="48" height="48" />
-        <div>
-          <b>Open 24/7</b>
-          <br />
-          Reserve a room anytime, day or night.
+      <div className="boxes">
+        <div className="box">
+          <img src="/time.png" alt="" width="48" height="48" />
+          <div>
+            <b>Open 24/7</b>
+            <br />
+            Reserve a room anytime, day or night.
+          </div>
+        </div>
+        <div className="box">
+          <img src="/book.png" alt="" width="48" height="48" />
+          <div>
+            <b>Quiet Spaces</b>
+            <br />
+            Find a calm room when you need to focus.
+          </div>
         </div>
       </div>
     </>
